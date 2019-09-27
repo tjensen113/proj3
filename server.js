839ceb024094df33b12058f87c31a6d43fdd1589
Connor Scoma <connorscoma@live.com>
2019-09-26 21:11:00 -0500

    adding cores

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+
+var cors = require('cors')
+app.use(cors())
+
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Access-Controller
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
